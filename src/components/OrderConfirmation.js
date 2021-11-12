98cdd8de2365157167a0327e6d911e8a518ae6e9
Your Name <tbengtson82@gmail.com>
2021-11-12 14:10:13 -0600

    built on submit and formValues and order states

--- src/components/OrderConfirmation.js
+++ src/components/OrderConfirmation.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 export default function OrderConfirmation(props) {
-
+    const { order } = props;
 
     return(
         <div>
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import OrderForm from "./OrderForm";
 import OrderConfirmation from "./OrderConfirmation";
+import axios from "axios";
 
 const initialFormValues = {
     name: "",
@@ -8,21 +9,50 @@ const initialFormValues = {
     pepperoni: false,
     sausage: false,
     bacon: false,
-    chicken: false
+    chicken: false,
+    special: "",
 }
 
 
 export default function Orders() {
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [formValues, setFormValues] = useState(initialFormValues);
+    const [order, setOrder] = useState({});
+    const values = formValues;
 
+    const placeOrder =newOrder => {
+        axios.post(`https://reqres.in/api/orders`)
+            .then(res => {
+                setOrder(newOrder);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+            .finally(() => {
+                setFormValues(initialFormValues);
+                setIsSubmitted(true);
+            })
+    }
+
+    const onSubmit = evt => {
+        const newOrder = {
+            name: values.name,
+            size: values.size,
+            pepperoni: values.pepperoni,
+            sausage: values.sausage,
+            bacon: values.bacon,
+            chicken: values.chicken,
+            special: values.special
+        }
+        placeOrder(newOrder);
+    }
 
 
     return(
         <div>
             {
                 !isSubmitted ? <OrderForm/>
-                    : <OrderConfirmation/>
+                    : <OrderConfirmation order={order}/>
             }
         </div>
     )
