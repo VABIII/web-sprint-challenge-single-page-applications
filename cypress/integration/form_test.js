25805ead9f56254474029ce553e461adb5dfd3d9
Your Name <tbengtson82@gmail.com>
2021-11-12 17:00:40 -0600

    all lambda tests passing

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,4 +1,21 @@
+describe("Lambda Eats", ()=>{
+    beforeEach(() => {
+        cy.visit(`http://localhost:3000/pizza`);
+    })
 
+    const nameInput = () => cy.get("input[name=name");
+
+    it("The proper elements are showing", () => {
+        nameInput().should("exist");
+    })
+
+
+
+
+
+
+
+})
 
 
 
